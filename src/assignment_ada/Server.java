131c47a2c5ee3177281fd7e8dfe4189f1b7656ee
valar <valar@DESKTOP-IRTLSMT>
2020-09-02 20:06:04 +1200

    NEW Assignment FILE

--- src/assignment_ada/Server.java
+++ src/assignment_ada/Server.java
@@ -12,107 +12,108 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author valar
  */
 public class Server {
-    
+
     public static final int HOST_PORT = 6554;
     private boolean stopRequested;
     private final String official_password = "COBRAKAI@123!";
     static Server server = new Server();
-    
+
     private Server() {
         stopRequested = false;
     }
-    
+
     public void startServer() {
         stopRequested = false;
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(HOST_PORT);
-        }
-        catch (IOException ex) {
-            System.err.println("error: "+ex);
+        } catch (IOException ex) {
+            System.err.println("error: " + ex);
             System.exit(-1);
         }
-        try
-        {
+        try {
             while (stopRequested == false) {
                 Socket socket = serverSocket.accept();
-                System.out.println("Connection made with"+socket.getInetAddress());
+                System.out.println("Connection made with" + socket.getInetAddress());
                 Writer writer = new Writer(socket);
                 Thread writerThread = new Thread(writer);
                 writerThread.start();
             }
             serverSocket.close();
-        }
-        catch (IOException ex) {
-            System.err.println("error: "+ex);
+        } catch (IOException ex) {
+            System.err.println("error: " + ex);
         }
     }
+
     //Writer class or method
-    private class Writer implements Runnable{
+    private class Writer implements Runnable {
 
         private Socket socket;
         private boolean connected;
         private PrintWriter writer;
-        
-        
+
         public Writer(Socket socket) {
             this.socket = socket;
             connected = true;
-            try{
-                writer = new PrintWriter(socket.getOutputStream(),true);
-            }
-            catch (IOException ex) {
+            try {
+                writer = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException ex) {
                 System.err.println("Error: " + ex);
             }
         }
-        
+
         @Override
         public void run() {
             writer.println("Testing");
             while (connected == true) {
-                
+
             }
             try {
                 socket.close();
                 writer.close();
-            }
-            catch (IOException ex) {
+            } catch (IOException ex) {
                 System.err.println("Error: " + ex);
             }
         }
-        
+
     }
+
     //Reader class 
-    private class Reader implements Runnable{
-        
+    private class Reader implements Runnable {
+
         private Socket sockets;
         private boolean connected = false;
         private BufferedReader reader;
-        
-        public Reader(Socket sockets){
+
+        public Reader(Socket sockets) {
             this.sockets = sockets;
             connected = true;
+
+            try {
+                reader = new BufferedReader(new InputStreamReader(sockets.getInputStream()));
+            } catch (IOException ex) {
+                System.err.println("Error: " + ex);
+            }
         }
-        
+
         @Override
         public void run() {
-            while(connected == true){
-                try {
-                    reader = new BufferedReader(new InputStreamReader(sockets.getInputStream()));
-                } catch (IOException ex) {
-                    System.err.println("Error: " + ex);
-                }
+            
+            while (connected == true) {
+                
+                
+                Thread thread = new Thread();
             }
-        }  
+        }
     }
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Server server = new Server();
         server.startServer();
     }
